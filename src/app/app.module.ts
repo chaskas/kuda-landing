378056e7be621ddbe09a432c59395fec6f481e41
Rodrigo Campos <rodrigo@webdevel.cl>
2017-05-07 15:26:15 -0300

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './modules/material/material.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdCardModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdGridListModule } from '@angular/material';
+import { MdTabsModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdSelectModule } from '@angular/material';
+import { MdAutocompleteModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdButtonModule,
+    MdIconModule,
+    MdGridListModule,
+    MdTabsModule,
+    MdInputModule,
+    MdSelectModule,
+    MdAutocompleteModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdButtonModule,
+    MdIconModule,
+    MdGridListModule,
+    MdTabsModule,
+    MdInputModule,
+    MdSelectModule,
+    MdAutocompleteModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
