5ea43aae4e9c8e20c8627880a25ccb0965f31738
Rodrigo Campos <rodrigo@webdevel.cl>
2017-05-12 00:10:04 -0300

    firebase updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBs_51YRSjGOVHpHTc9WKGSjPCBcrVsPEI",
+    authDomain: "kuda-landing-a73e4.firebaseapp.com",
+    databaseURL: "https://kuda-landing-a73e4.firebaseio.com",
+    projectId: "kuda-landing-a73e4",
+    storageBucket: "kuda-landing-a73e4.appspot.com",
+    messagingSenderId: "343484888976"
+  }
 };
